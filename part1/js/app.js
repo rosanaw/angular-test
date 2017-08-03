12834a24e688313954e04304dd07d56c58f5d288
rosana <rosana.waszak@base.digital>
2017-08-03 14:43:53 -0300

    Fix variables

--- part1/js/app.js
+++ part1/js/app.js
@@ -8,15 +8,11 @@
         var selectCtrl = this;
 
         selectCtrl.theVar = "";
-        selectCtrl.theVarInput = "";
         selectCtrl.theVarSelect = ["one", "two", "three", "four", "five"];
-        selectCtrl.selectedOption = "";
 
         selectCtrl.updateValue = function(value) {
-            selectCtrl.theVar = value;
-            selectCtrl.theVarInput = value;
             if(selectCtrl.theVarSelect.indexOf(value) != -1) {
-                selectCtrl.selectedOption = value;
+                selectCtrl.theVar = value;
             }
         };
 
